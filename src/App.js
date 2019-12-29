df3fd580c4c5f0af14dd665aaf11bbbb9931a52c
titansarus <amirm137878@gmail.com>
2019-12-29 15:56:27 +0330

    Sign In Sign Up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import Navbar from "./template_compnent/Navbar";
 import './App.css';
+import Sign from './components/Sign.js'
+import {Route, BrowserRouter} from 'react-router-dom'
+import SignUp from "./components/SignUp";
 
 class App extends Component {
     render() {
         return (
-            <Navbar/>
+            <BrowserRouter>
+                <Navbar/>
+                <Route path="/"/>
+                <Route exact path="/sign" render={() => <Sign/>}/>
+                <Route exact path="/signUp" render={() => <SignUp/>}/>
+            </BrowserRouter>
 
 
         )
--- src/template_compnent/Navbar.js
+++ src/template_compnent/Navbar.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import logo from "../static/img/reddit-logo.png"
+import {Link} from 'react-router-dom'
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/../static/img/reddit-logo"><img style={{width: "40px", height:"40px"}} src={logo}/></a>
+                <a className="navbar-brand" href="/"><img
+                    style={{width: "40px", height: "40px"}} src={logo}/></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -13,9 +15,9 @@ class Navbar extends Component {
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav  mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
@@ -33,11 +35,22 @@ class Navbar extends Component {
                             </div>
                         </li>
 
+
                     </ul>
+
+
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
+                    <ul className="form-inline my-2 my-lg-0 navbar-nav" style={{margin: "10px"}}>
+                        <li className="nav-item btn-primary btn-sm" style={{margin: "5px"}}>
+                            <a className="nav-link" href="/sign" style={{color: "white"}}>Sign In</a>
+                        </li>
+                        <li className="nav-item btn-success btn-sm">
+                            <a className="nav-link" href="/signUp" style={{color: "white"}}>Sign Up</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
 
