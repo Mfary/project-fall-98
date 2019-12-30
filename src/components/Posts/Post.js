58c31001f96e5c6e982c32d6b170b4f2f08ee0a7
Amirmahdi Namjoo <amirm137878@gmail.com>
2019-12-31 01:21:23 +0330

    A lot of changes. topics. not complete but fine.

---
+++ src/components/Posts/Post.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+
+class Post extends Component {
+    state={
+        content: "",
+        author: "",
+        date: "",
+        order: "0",
+        id : "",
+        comments:[]
+
+
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+
+export default Post;
