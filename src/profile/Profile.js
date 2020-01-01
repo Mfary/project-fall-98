ac839bb8a895388b955aafa5a472413daf74373a
mfary <moha1379>
2020-01-01 16:56:23 +0330

    change in profile

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Follow from "./follow";
 import Edit from "./edit";
+import "./style.css"
 
 class Profile extends Component {
     render() {
@@ -13,29 +14,28 @@ class Profile extends Component {
             <div className="container">
                 <div className="row">
                     <div className="card col-8" style={{ margin : "auto"  }}>
-                        <img className="card-img-top" src={this.props.image} alt=""/>
                         <div className="card-body">
+                            <img className="col-3 img" src={this.props.image} alt=""/>
                             <h2 className="card-title">{this.props.name}</h2>
-                            {button}
                             <div className="row">
-                                <div className="col-4">
+                                <div className="col-3 item">
                                     follwers
                                 </div>
-                                <div className="col-4">
+                                <div className="col-3 item">
                                     follwing
                                 </div>
-                                <div className="col-4">
+                                <div className="col-3 item">
                                     posts
                                 </div>
                             </div>
                             <div className="row">
-                                <div className="col-4">
+                                <div className="col-3 item">
                                     {this.props.followers}
                                 </div>
-                                <div className="col-4">
+                                <div className="col-3 item">
                                     {this.props.following}
                                 </div>
-                                <div className="col-4">
+                                <div className="col-3 item">
                                     {this.props.posts}
                                 </div>
                             </div>
@@ -43,6 +43,7 @@ class Profile extends Component {
                     </div>
                 </div>
                 <div id="posts"></div>
+                <sidebar />
             </div>
         )
     }
--- src/profile/edit.js
+++ src/profile/edit.js
@@ -4,12 +4,19 @@ import "./style.css"
 class Edit extends Component {
     render() {
         return (
-            <div className="container">
-                <a href="#" class="btn btn-primary">Edit profile</a>
-                <a href="#" class="btn btn-primary">Change password</a>
-                <a href="#" class="btn btn-primary">Post</a>
+            <div className="button-container col-9">
+                <div>
+                    <a href="#" class="btn btn-primary">Edit profile</a>
+                </div>
+                <div>
+                    <a href="#" class="btn btn-primary">Change password</a>
+                </div>
+                <div>
+                    <a href="#" class="btn btn-primary">Post</a>
+                </div>
             </div>
         )
     }
 }
+
 export default Edit
