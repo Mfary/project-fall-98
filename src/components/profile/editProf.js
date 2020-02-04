a89dbab393b88ae32bf710f81ceccefb9fa21e7f
mfary <moha1379>
2020-02-04 20:16:41 +0330

    update user but can not send image because we have not form

--- src/components/profile/editProf.js
+++ src/components/profile/editProf.js
@@ -6,6 +6,7 @@ class EditProf extends Component {
         super();
         this.submit = this.submit.bind(this)
     }
+
     async submit() {
         let username = document.getElementById("username").value;
         let email = document.getElementById("email").value;
@@ -19,21 +20,26 @@ class EditProf extends Component {
 
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization","Bearer "+localStorage.getItem("ACCESS_TOKEN"))
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("ACCESS_TOKEN"))
 
-        var raw = JSON.stringify({
-            "email": email,
-            "first_name": first_name,
-            "last_name": last_name,
-            "phone_number": phone_number,
-            "city": city,
-            "username": username,
-            "country": country,
-            "picture": picture,
-        });
+        if (!(document.getElementById("username").validity.valid && document.getElementById("email").validity.valid && document.getElementById("last_name").validity.valid && document.getElementById("first_name").validity && document.getElementById("phone_number").validity.valid &&document.getElementById("pic").validity.valid &&document.getElementById("city").validity.valid && document.getElementById("country").validity.valid)){
+            alert("some required input is not given")
+            return
+        }
+
+            var raw = JSON.stringify({
+                "email": email,
+                "first_name": first_name,
+                "last_name": last_name,
+                "phone_number": phone_number,
+                "city": city,
+                "username": username,
+                "country": country,
+                "picture": picture,
+            });
 
         var requestOptions = {
-            method: 'POST',
+            method: 'PUT',
             headers: myHeaders,
             body: raw,
             redirect: 'follow'
@@ -49,6 +55,7 @@ class EditProf extends Component {
             .catch(error => alert('error' + error));
 
     }
+
     render() {
         return (
             <div className="container">
@@ -59,27 +66,27 @@ class EditProf extends Component {
                             <div className="form-group row">
                                 <label htmlFor="name">Username</label>
                                 <input type="text" className="form-control" id="username"
-                                       placeholder="username"/>
+                                       placeholder="username" required/>
                             </div>
                             <div className="form-group row">
                                 <label htmlFor="name">First name</label>
                                 <input type="text" className="form-control" id="first_name"
-                                       placeholder="first_name"/>
+                                       placeholder="first_name" required/>
                             </div>
                             <div className="form-group row">
                                 <label htmlFor="name">Last name</label>
                                 <input type="text" className="form-control" id="last_name"
-                                       placeholder="last_name"/>
+                                       placeholder="last_name" required/>
                             </div>
                             <div className="form-group row">
                                 <label htmlFor="phoneNumber">Phone Number</label>
                                 <input type="tel" className="form-control" id="phone_number"
-                                       placeholder="phone number"/>
+                                       placeholder="phone number" required/>
                             </div>
                             <div className="form-group row">
                                 <label htmlFor="email">Email</label>
                                 <input type="email" className="form-control" id="email"
-                                       placeholder="mail@mail.com"/>
+                                       placeholder="mail@mail.com" required/>
                             </div>
                             <div className="form-group row">
                                 <label htmlFor="country">Country</label>
@@ -94,10 +101,11 @@ class EditProf extends Component {
                             <div className="form-group row">
                                 <label htmlFor="pic">Picture</label>
                                 <input type="file" className="form-control" id="pic" accept="image/*"
-                                       placeholder="picture"/>
+                                       formenctype="multipart/form-data"/>
                             </div>
                             <div className="center">
-                                <button onClick={this.submit} type="submit" className="btn btn-primary col-6">Edit</button>
+                                <button onClick={this.submit} type="submit" className="btn btn-primary col-6">Edit
+                                </button>
                             </div>
                         </div>
                     </div>
