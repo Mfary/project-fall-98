da5638049bcf7439ea55ee7952ceb5399e69572c
mfary <moha1379>
2020-02-04 18:49:17 +0330

    make channel without test

--- src/components/Channel/channelPage.js
+++ src/components/Channel/channelPage.js
@@ -3,6 +3,55 @@ import channels from '../../static/jsons/Channels'
 import ChannelCard from "../profile/ChannelCard";
 
 class ChannelPage extends Component {
+    constructor() {
+        super();
+        this.insideSubmit = this.insideSubmit.bind(this)
+    }
+    async insideSubmit() {
+
+        let username = document.getElementById("name").value;
+        let title = document.getElementById("title").value;
+        let rules = document.getElementById("rule").value;
+        let desc = document.getElementById("description").value;
+
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        if (!(document.getElementById("title").validity.valid && document.getElementById("name").validity.valid && document.getElementById("title").validity.valid && document.getElementById("desc").validity.valid)) {
+            alert("something in input is wrong")
+            return
+        }
+
+        var raw = JSON.stringify({
+            "law": rules,
+            "identifier": username,
+            "description": desc,
+            "title": title,
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("http://127.0.0.1:8000/api/chanel/", requestOptions)
+            .then(response => response.text())
+            .then(function (result) {
+                console.log(result)
+                let obj = JSON.parse(result);
+                let msg = obj.msg;
+                alert(msg)
+
+            })
+            .catch(error => {
+                alert('error' + error)
+            });
+
+
+    }
 
     state = {
         channels: []
@@ -44,39 +93,36 @@ class ChannelPage extends Component {
                         aria-expanded="false" aria-controls="collapseExample">
                     Make New Channel
                 </button>
-                <div className={"collapse"} id={"collapseInputForm"}>
-                    <form>
-                        <div>
-                            <div className="md-form">
-                                <label htmlFor="form1">Name</label>
-                                <input type="text" id="form1" className="form-control"
-                                />
-                            </div>
-                            <div className="md-form">
-                                <label htmlFor="form1">Title</label>
-                                <input type="text" id="form1" className="form-control"
-                                />
-                            </div>
-                            <div className="md-form">
-                                <label htmlFor="form1">Rules</label>
-                                <input type="text" id="form1" className="form-control"
-                                />
-                            </div>
+                <div className="collapse" id="collapseInputForm">
+                    <div>
+                        <div className="md-form">
+                            <label htmlFor="form1">Name</label>
+                            <input type="text" id="name" className="form-control" placeholder="name"
+                            />
                         </div>
-                        <div>
-                            <div className="form-group shadow-textarea">
-                                <label htmlFor="exampleFormControlTextarea6">Content</label>
-                                <textarea className="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3"
-                                          placeholder="Write something here to make a new channel"
-                                          value={this.state.content}
-                                ></textarea>
-                            </div>
+                        <div className="md-form">
+                            <label htmlFor="form1">Title</label>
+                            <input type="text" id="title" className="form-control" placeholder="ittle"
+                            />
+                        </div>
+                        <div className="md-form">
+                            <label htmlFor="form1">Rules</label>
+                            <input type="text" id="rules" className="form-control" placeholder="rules"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="form-group shadow-textarea">
+                            <label htmlFor="decription">Content</label>
+                            <textarea className="form-control z-depth-1" id="description" rows="3"
+                                      placeholder="Write something here to make a new channel"
+                                      value="decribe your channel"></textarea>
+                        </div>
 
-                        </div>
-                        <button type="submit" className="btn btn-labeled btn-success">
-                            <span><i className=""></i></span>Make
-                        </button>
-                    </form>
+                    </div>
+                    <button onClick={this.insideSubmit} type="submit" className="btn btn-primary">
+                        Make
+                    </button>
                 </div>
 
             </div>
