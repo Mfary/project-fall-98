74f2b8e38833a028df5b3360d87d2817a59bbbe8
titansarus <amirm137878@gmail.com>
2020-02-04 12:42:31 +0330

    REDIRECT to FIRST PAGE

--- src/components/Signs/Sign.js
+++ src/components/Signs/Sign.js
@@ -1,50 +1,68 @@
 import React, {Component} from 'react'
 import "./sign.css"
+import {Redirect} from "react-router-dom";
 
-async function submit() {
-
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    if(!(document.getElementById("password").validity.valid && document.getElementById("username").validity.valid)){
-        alert("something in input is wrong")
-        return
-    }
-
-    var raw = JSON.stringify({
-        "password": password,
-        "username": username,
-    });
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    fetch("http://127.0.0.1:8000/api/account/login", requestOptions)
-        .then(response => response.text())
-        .then(function (result) {
-            console.log(result)
-            console.log(localStorage.getItem("ACCESS_TOKEN"))
-            let obj = JSON.parse(result);
-            let access = obj.access;
-            localStorage.setItem("ACCESS_TOKEN", access)
-
-            console.log(localStorage.getItem("ACCESS_TOKEN"))
-            console.log("OK");
-        })
-        .catch(error => alert('error'+error));
-
-}
 
 class Sign extends Component {
+
+    async insideSubmit() {
+        let thisIS = this;
+
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        if (!(document.getElementById("password").validity.valid && document.getElementById("username").validity.valid)) {
+            alert("something in input is wrong")
+            return
+        }
+
+        var raw = JSON.stringify({
+            "password": password,
+            "username": username,
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("http://127.0.0.1:8000/api/account/login", requestOptions)
+            .then(response => response.text())
+            .then(function (result) {
+                console.log(result)
+                console.log(localStorage.getItem("ACCESS_TOKEN"))
+                let obj = JSON.parse(result);
+                let access = obj.access;
+                if (access != undefined) {
+                    localStorage.setItem("ACCESS_TOKEN", access)
+                }
+
+                console.log(localStorage.getItem("ACCESS_TOKEN"))
+                console.log("OK");
+                thisIS.forceUpdate();
+
+            })
+            .catch(error => {
+                alert('error' + error)
+                localStorage.removeItem("ACCESS_TOKEN")
+                thisIS.forceUpdate();
+            });
+
+
+    }
+
+
     render() {
+
+        if (localStorage.getItem("ACCESS_TOKEN") != null && localStorage.getItem("ACCESS_TOKEN") != undefined) {
+            return <Redirect to='/'/>
+        }
         console.log("Hello")
         return (
             <div id="signUpContainer">
@@ -75,11 +93,8 @@ class Sign extends Component {
                                     <input type="checkbox"/><span style={{color: "black"}}>Remember Me</span>
                                 </div>
                                 <div className="form-group">
-                                    <input onClick={function () {
-                                        console.log("BUTTON ON CLICK")
-                                        submit()
-
-                                    }} type="submit" value="Login" className="btn float-right login_btn"/>
+                                    <input onClick={this.insideSubmit.bind(this)} type="submit" value="Login"
+                                           className="btn float-right login_btn"/>
                                 </div>
 
                             </div>
